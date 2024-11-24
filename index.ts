423feb6c8b01b23f3197c42af2e0ea4bc6a4163b
ururu42 <maltceva.law@gmail.com>
2024-11-24 23:56:41 +0300

    Connected to Whisper LLM

--- index.ts
+++ index.ts
@@ -1,18 +1,39 @@
-import { Bot, Context } from 'grammy'
-import { FileFlavor, hydrateFiles } from '@grammyjs/files'
-
 import 'dotenv/config'
+
+import fetch, { fileFrom } from 'node-fetch'
+
+import { Bot, Context, session, SessionFlavor } from 'grammy'
+import { FileFlavor, hydrateFiles } from '@grammyjs/files'
+import { FileAdapter } from '@grammyjs/storage-file'
+
+// TODO: заюзать Local Bot API Server
+// @see https://grammy.dev/guide/api#running-a-local-bot-api-server
+
+interface SessionData {
+    // counter: number;
+}
 
 if (!process.env.TOKEN) {
     throw new Error('TOKEN is not defined')
 }
 
 // Transformative Context flavor
-type MyContext = FileFlavor<Context>
+type MyContext = FileFlavor<Context> & SessionFlavor<SessionData>
 
 const bot = new Bot<MyContext>(process.env.TOKEN)
 
 bot.api.config.use(hydrateFiles(bot.token))
+
+bot.use(
+    session({
+        initial: () => ({
+            // counter: 0,
+        }),
+        storage: new FileAdapter({
+            dirName: 'sessions'
+        })
+    })
+)
 
 bot.command('start', async (ctx) => {
     return await ctx.reply('Hi!')
@@ -30,7 +51,24 @@ bot.on('message:voice', async (ctx) => {
     const path = await file.download()
     console.log('File saved at ', path)
 
-    // or file.getUrl()
+    const formData = new FormData()
+    formData.append('audio_file', await fileFrom(path))
+
+    try {
+        const response = await fetch('http://localhost:9000/asr', {
+            method: 'POST',
+            headers: {},
+            body: formData
+        })
+
+        console.log(await response.text())
+    } catch (error: any) {
+        console.log(`Error: ${error}`)
+    }
+
+    // TODO: удалять скачанный файл
+
+    // return await ctx.replyWithVoice()
 })
 
 bot.start()
