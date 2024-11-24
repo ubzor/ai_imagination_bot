924fbf348de3d24b2f1baa54c93d932f410a8d60
ururu42 <maltceva.law@gmail.com>
2024-11-24 13:48:31 +0300

    Getting voice messages

--- index.ts
+++ index.ts
@@ -1,20 +1,36 @@
-import { Bot } from 'grammy'
+import { Bot, Context } from 'grammy'
+import { FileFlavor, hydrateFiles } from '@grammyjs/files'
+
 import 'dotenv/config'
 
 if (!process.env.TOKEN) {
     throw new Error('TOKEN is not defined')
 }
 
-const bot = new Bot(process.env.TOKEN) // <-- put your bot token between the "" (https://t.me/BotFather)
+// Transformative Context flavor
+type MyContext = FileFlavor<Context>
 
-bot.command('start', (ctx) => {
-    return ctx.reply('Hi!')
+const bot = new Bot<MyContext>(process.env.TOKEN)
+
+bot.api.config.use(hydrateFiles(bot.token))
+
+bot.command('start', async (ctx) => {
+    return await ctx.reply('Hi!')
 })
 
-// Reply to any message with "Hi there!".
-bot.on('message', (ctx) => {
+bot.on('message:text', async (ctx) => {
     console.log(`User sent a message: ${ctx.update.message.text}`)
-    return ctx.reply('Hi there!')
+    return await ctx.reply('Hi there!')
+})
+
+bot.on('message:voice', async (ctx) => {
+    console.log(`User sent a voice message`, ctx.update.message.voice)
+
+    const file = await ctx.getFile()
+    const path = await file.download()
+    console.log('File saved at ', path)
+
+    // or file.getUrl()
 })
 
 bot.start()
